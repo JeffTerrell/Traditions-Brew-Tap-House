58c84aef93ed34a6b303416019aa23a644b74c33
Jeff Terrell <jeffaterrell@gmail.com>
2022-02-11 18:55:54 -0500

    add kegDetail component

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,14 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function KegDetail(props) {
   return(
     <React.Fragment>
       <div id="keg-detail">
         <h3>Keg Detail</h3>
-      </div>
-      
+        <h6>Name: {props.name}</h6>
+        <h6>Brewery: {props.brewery}</h6>
+        <h6>Type: {props.type}</h6>
+        <h6>ABV: {props.abv}</h6>
+        <h6>Price: {props.price}</h6>
+      </div>   
     </React.Fragment>
   );
 }
+
+KegDetail.propTypes = {
+  KegDetail: PropTypes.object
+};
 
 export default KegDetail;
