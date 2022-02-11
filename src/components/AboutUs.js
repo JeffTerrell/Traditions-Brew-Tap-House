9439d9929652b07653b44dd24167cc0106e2764d
Jeff Terrell <jeffaterrell@gmail.com>
2022-02-11 15:57:56 -0500

    WIP: connecting button links from NavBar to change current view

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,8 +5,9 @@ function AboutUs(){
   return(
     <React.Fragment>
       <div id="home">
-        <h3>Sample Header</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus pretium rutrum. Aliquam vel tortor elementum, laoreet tortor eleifend, volutpat elit. Aenean ac pharetra nibh. Donec sed aliquet turpis. Nam dolor eros, sagittis lobortis porta ut, fringilla et justo. Integer porttitor, dolor ac laoreet dapibus, enim urna elementum ante, a sodales orci nibh id ligula. Mauris pulvinar enim condimentum tincidunt eleifend. Duis id diam vel neque cursus commodo. Sed et nulla ut nisl tempus hendrerit nec ut odio. Maecenas lacus ante, gravida a urna et, consequat ullamcorper augue. Integer imperdiet orci aliquam neque interdum mollis sed quis ligula. Nulla blandit nibh at nisi condimentum, at vulputate magna volutpat. Etiam porttitor metus euismod, faucibus risus et, semper ligula. Duis euismod laoreet lorem, vel cursus nisi porttitor et. In non orci vestibulum, eleifend dui non, bibendum augue. Vestibulum tristique, velit id facilisis porttitor, sapien massa tempus sapien, a volutpat lectus enim sed neque. Quisque sagittis, nulla tristique scelerisque laoreet, dolor sapien condimentum tortor, sit amet lobortis urna magna vel lacus. Sed feugiat ante faucibus dolor tempor.</p>
+        <h3>Welcome!</h3>
+        <p>Come on in and enjoy our delicious offerings from some of the best breweries in the world!</p>
+        <p>Local brews are also available year round!</p>
       </div>
     </React.Fragment>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import woodKeg from "./../img/woodKeg.jpg";
 
-function NavBar(){
+function NavBar(props){
+
+  const test = props.handleClick;
+
   return (
     <React.Fragment>
       <div id="navbar">
         <h1>Traditions Brew Tap House</h1>
         <img src={woodKeg} alt="wood keg"/>
+        <button onClick={()=> test("home")}>Home</button>  
+        <button onClick={()=> test("KegList")}>Keg List</button>  
+        <button onClick={()=> test("NewKegForm")}>Add Keg</button>
       </div>
     </React.Fragment>
   );
